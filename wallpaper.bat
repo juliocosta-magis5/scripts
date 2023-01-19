7bbbe12e1807226ec0f58c08be4cccfabf05a5e4
juliocosta-magis5 <117852371+juliocosta-magis5@users.noreply.github.com>
2023-01-19 11:47:28 -0300

    Delete wallpaper.bat

--- wallpaper.bat
+++
@@ -1,8 +0,0 @@
-#acessando diretorio temp
-cd C:\Users\Public\Downloads
-
-#baixando imagem
-curl https://storage.googleapis.com/magis5-storage-1/images/desktop.jpg -o desktop.jpg
-
-#aplicando imagem
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d C:\Users\Public\Downloads\desktop.jpg
